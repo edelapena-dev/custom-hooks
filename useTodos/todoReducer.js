12c9ff6aa2d86c10c03bea0b451b37c890ed3518
YyzRush <YyzRush@DESKTOP-8411MDB>
2022-07-21 17:10:32 -0400

    useTodo agregado correctamente

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,23 @@
+// { type: [TODO] Remove Todo, payload: id}
+export const todoReducer = (initialState, action) => {
+    switch (action.type) {
+        case '[TODO] Add Todo':
+            return [ ...initialState, action.payload];
+             // throw new Error ('Action.type = ABC no esta iomplementada');
+        case '[TODO] Remove Todo':
+            return initialState.filter( item => item.id !== action.payload);
+        
+        case '[TODO] Toggle Todo':
+            return initialState.map( item => {
+                if(item.id === action.payload) {
+                    return { ...item, 
+                            done: !item.done }
+                }
+                return item;
+            });
+
+        default:
+            return initialState;
+    }
+
+}
